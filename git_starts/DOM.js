2c3d60dc94c38ac31c02bf910a161d6b8707b360
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-04-08 11:05:48 +0530

    second code to git using VS

---
+++ git_starts/DOM.js
@@ -0,0 +1,7 @@
+const header=document.getElementById('header-title');
+
+header.style.border= "solid 3px #000"
+
+const header2=document.getElementById("main").getElementsByClassName("title")[0]
+
+header2.style.color="green"
